9314f2e3ac6c91d3ebfee491f56c642ee20f3734
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-06 02:11:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require "pry"
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade_level)
+    @student = student
+    @grade_level = grade_level
+    if roster.keys.include?(@grade_level)
+      roster[@grade_level] << @student
+     
+    else
+       roster[grade_level] = []
+       roster[grade_level] << @student
+    end
+  end
+  
+  def grade(grade_level)
+    @grade_level = grade_level
+    if roster.keys.include?(@grade_level)
+     roster[grade_level]
+    end
+   end
+   
+   def sort
+       @roster[@grade_level](@student).sort
+       end
+   
+end
